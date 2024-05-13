4337b4d19315731785c6a9b3a14841789700964c
Shril Kumar <14113412+shril@users.noreply.github.com>
2024-05-13 21:53:06 +0530

    added feature

--- src/cron_parser/cron_expression.py
+++ src/cron_parser/cron_expression.py
@@ -3,15 +3,24 @@ from cron_fields import MinuteField, HourField, DayOfWeekField, MonthField, DayO
 
 class CronExpression:
     @staticmethod
+    def process_command(command) -> str:
+        main_command = command[0]
+        sub_commands = []
+        for i in range(1, len(command), 2):
+            sub_commands.append(f'{command[i].replace("-", "")}:{command[i + 1]}')
+        return f'{main_command} {" ".join(sub_commands)}'
+
+    @staticmethod
     def get_fields_and_values(cron_string: str) -> dict:
-        minute, hour, day_of_month, month, day_of_week, command = cron_string.split()
+        minute, hour, day_of_month, month, day_of_week, *command = cron_string.split()
+        process_command = CronExpression.process_command(command) if len(command) > 0 else command
         return {
             'minute': MinuteField(minute).get_values(),
             'hour': HourField(hour).get_values(),
             'day of month': DayOfMonthField(day_of_month).get_values(),
             'month': MonthField(month).get_values(),
             'day of week': DayOfWeekField(day_of_week).get_values(),
-            'command': command
+            'command': process_command
         }
 
     @staticmethod
