1b7eadefd24d5bdfc7978df766f2e6ba8af1c716
Shril Kumar <14113412+shril@users.noreply.github.com>
2024-05-14 00:56:35 +0530

    Changed logic

--- src/cron_parser/cron_expression.py
+++ src/cron_parser/cron_expression.py
@@ -1,33 +1,30 @@
 import re
-from cron_fields import MinuteField, HourField, DayOfWeekField, MonthField, DayOfMonthField
+from cron_fields import MinuteField, HourField, DayOfWeekField, MonthField, DayOfMonthField, YearField
 
 
 class CronExpression:
     @staticmethod
     def get_fields_and_values(cron_string: str) -> dict:
         minute, hour, day_of_month, month, day_of_week, *command = cron_string.split()
-        year = None
-        if len(command) > 1 and command[0].isdigit():
-            year, *new_command = command[0], command[1:]
-        else:
-            new_command = command
-        return {
+        result = {
             'minute': MinuteField(minute).get_values(),
             'hour': HourField(hour).get_values(),
             'day of month': DayOfMonthField(day_of_month).get_values(),
-            'month': MonthField(month).get_values(),
             'day of week': DayOfWeekField(day_of_week).get_values(),
-            'year': year,
-            'command': new_command
+            'month': MonthField(month).get_values(),
+            'command': command
         }
+        if len(command) > 1 and command[0][:4].isdigit():
+            year, *new_command = command[0], command[1]
+            result['year'] = YearField(year).get_values()
+            result['command'] = new_command[0]
+        return result
 
     @staticmethod
     def generate_output(fields_map: dict) -> str:
         lines = []
         for field, values in fields_map.items():
             if field == 'command':
-                line = f'{field:<14} {values}'
-            elif field == 'year':
                 line = f'{field:<14} {values}'
             else:
                 line = f'{field:<14} {" ".join(map(str, values))}'
